486afc2dbd2c0d13b03919587b518bb206f6ec37
Roman <rio900@icloud.com>
2025-05-15 02:38:41 +0300

    Added debug information for connection status and a parser for custom events.

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -3,23 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using DotStriker.NetApiExt;
 using DotStriker.NetApiExt.Generated;
-using UnityEditor.Networking.PlayerConnection;
 using UnityEngine;
-using Substrate.NetApi;
 using Substrate.NetApi.Model.Extrinsics;
 using Substrate.NetApi.Model.Types;
 using DotStriker.Integration.Client;
 using DotStriker.NetApiExt.Generated.Model.solochain_template_runtime;
 using DotStriker.NetApiExt.Generated.Model.pallet_template.pallet;
-
+using Event = DotStriker.NetApiExt.Generated.Model.pallet_template.pallet.Event;
+using System.Linq;
+using Substrate.NetApi.Model.Types.Primitive;
+using TMPro;
+using Unity.VisualScripting;
 
 public class NetworkManager : MonoBehaviour
 {
     public string Url => "ws://127.0.0.1:9944";
+
+
+    [SerializeField]
+    TMP_Text _nodeUrlText;
+
+    [SerializeField]
+    TMP_Text _blockNumberText;
+
+    [SerializeField]
+    TMP_Text _updateNumberText;
+
+    [SerializeField]
+    TMP_Text _numberInsideTestEventText;
+
+    [SerializeField]
+    GameObject _debugInfoPanel;
+
     SubstrateClientExt _client;
     Account _ownerAccount;
+
+    int _updateNumber = 0;
 
     async void Start()
     {
@@ -27,38 +47,57 @@ public class NetworkManager : MonoBehaviour
         _client = new SubstrateClientExt(new Uri(Url),
                                         ChargeTransactionPayment.Default());
 
+        _nodeUrlText.text = Url;
         await Connect();
-        Debug.Log("Connected to node");
-        InvokeRepeating(nameof(UpdateState), 1.0f, 1.0f);
 
+        InvokeRepeating(nameof(UpdateState), 1.0f, 1.0f);
     }
 
     private async void UpdateState()
     {
+        _updateNumber++;
+        _updateNumberText.text = "Update number: " + _updateNumber.ToString();
+
         if (_client == null || !_client.IsConnected)
         {
-            Debug.Log("Client is null or disposed");
+            Debug.Log("[NetworkManager] [UpdateState] Client is null or disposed");
             return;
         }
 
-        Debug.Log($"UpdateState()");
+        var block = await _client.Chain.GetBlockAsync(CancellationToken.None);
+        _blockNumberText.text = "Block number: " + block.Block.Header.Number.ToString();
 
+        Debug.Log($"[NetworkManager] [UpdateState] Starting to get events");
         var events = await _client.SystemStorage.Events(null, CancellationToken.None);
-
-        var toArr = events.Value;
+        var toArr = events.Value.ToArray();
 
         foreach (var record in toArr)
         {
             var runtimeEvent = record.Event;
-            Debug.Log($"[UpdateState] Enevent: {runtimeEvent}");
+            Debug.Log($"[NetworkManager] [UpdateState] Enevent: {runtimeEvent}");
+
+            if (runtimeEvent.Value == RuntimeEvent.Template)
+            {
+                var templateEvent = runtimeEvent.Value2 as EnumEvent;
+
+                switch (templateEvent.Value)
+                {
+                    case Event.TestEvent:
+                        var tuple1 = templateEvent.Value2 as U32;
+                        _numberInsideTestEventText.text = "Test event:" + tuple1.ConvertTo<int>().ToString();
+
+                        Debug.Log($"[NetworkManager] [UpdateState] TestEvent {tuple1}");
+                        break;
+                }
+            }
         }
     }
 
     private async Task Connect()
     {
-        Debug.Log("Connecting to node...");
+        Debug.Log("[NetworkManager] [Connect] Connecting to node...");
         await _client.ConnectAsync(true, true, CancellationToken.None);
-        Debug.Log("Connected to node");
+        Debug.Log("[NetworkManager] [Connect] Connected to node");
     }
 
     internal Account GetAccount()
@@ -71,8 +110,15 @@ public class NetworkManager : MonoBehaviour
         return _ownerAccount;
     }
 
-    void Update()
+    public void OnDebugClick()
     {
-
+        if (_debugInfoPanel.activeInHierarchy == false)
+        {
+            _debugInfoPanel.SetActive(true);
+        }
+        else
+        {
+            _debugInfoPanel.SetActive(false);
+        }
     }
 }
